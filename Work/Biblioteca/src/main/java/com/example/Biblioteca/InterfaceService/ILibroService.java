603e05291016ae8bea72f74bda40461e203c410d
Cristian Bahamon <Cbahamongranados@gmail.com>
2024-06-20 17:08:45 -0500

    Se hace una carpeta mas organizada para el taller y se sube todo lo que se ha hecho hasta el momento

---
+++ Work/Biblioteca/src/main/java/com/example/Biblioteca/InterfaceService/ILibroService.java
@@ -0,0 +1,14 @@
+package com.example.Biblioteca.InterfaceService;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.Biblioteca.Models.libro;
+
+public interface ILibroService {
+    public String save(libro libro);    
+    public List<libro> findAll();
+    public Optional<libro> findOne(String id);
+    public int delete(String id);
+    public List<libro> filtroLibros(String filtro);
+}
