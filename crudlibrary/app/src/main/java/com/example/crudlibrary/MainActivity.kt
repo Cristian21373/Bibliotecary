71840d67911ed1e2bf6658bb33b69753ef60edc2
Cristian Bahamon <Cbahamongranados@gmail.com>
2024-07-31 17:38:30 -0500

    Se suben nuevos cambios al diÃseño

--- crudlibrary/app/src/main/java/com/example/crudlibrary/MainActivity.kt
+++ crudlibrary/app/src/main/java/com/example/crudlibrary/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.crudlibrary
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,38 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        var btnLibro =
+            findViewById<Button>(R.id.btnLibro)
+
+        var NuevoLibro =
+            findViewById<Button>(R.id.NuevoLibro)
+
+        btnLibro.setOnClickListener {
+            crudlibrary(1)
+        }
+
+        NuevoLibro.setOnClickListener {
+            crudlibrary(2)
+        }
+
+
+
+    }
+        fun crudlibrary(position:Int){
+            var fragment: Fragment
+            when(position){
+                1->fragment=opcionesLibro()
+                2->fragment=NuevoLibro()
+
+                else-> fragment = opcionesLibro()
+            }
+            val fragmentManager = supportFragmentManager
+
+            val fragmentTransition = fragmentManager.beginTransaction()
+            fragmentTransition.replace(R.id.FCV,fragment)
+            fragmentTransition.commit()
 
     }
 }
